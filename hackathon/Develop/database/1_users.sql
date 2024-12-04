6e66b72aa79585a6f916311ef44331c26a0fde9f
IrieAkari <irie040336@gmail.com>
2024-12-04 21:41:32 +0900

    add hackathon

--- hackathon/Develop/database/1_users.sql
+++ hackathon/Develop/database/1_users.sql
@@ -4,12 +4,11 @@ CREATE TABLE users (
     id CHAR(26) NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 -- 初期データの挿入
-INSERT INTO users (id, name, email, password) VALUES 
-('00000000000000000000000001', 'hanako', 'hanako@example.com', 'hashed_password_1'),
-('00000000000000000000000002', 'taro', 'taro@example.com', 'hashed_password_2');
+INSERT INTO users (id, name, email) VALUES 
+('00000000000000000000000001', 'hanako', 'hanako@example.com'),
+('00000000000000000000000002', 'taro', 'taro@example.com');
