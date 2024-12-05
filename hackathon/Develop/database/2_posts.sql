fd55421e1d5a820d38702cae30005496ab48ca64
IrieAkari <irie040336@gmail.com>
2024-12-06 05:26:19 +0900

    add hackathon

--- hackathon/Develop/database/2_posts.sql
+++ hackathon/Develop/database/2_posts.sql
@@ -15,9 +15,9 @@ CREATE TABLE posts (
 
 -- 初期データの挿入
 INSERT INTO posts (id, parent_id, user_id, content, likes_count, replys_count) VALUES 
-('00000000000000000000000001', NULL, '00000000000000000000000001', 'This is Hanako first post', 1, 1),
-('00000000000000000000000002', '00000000000000000000000001', '00000000000000000000000002', 'This is Taro reply to Hanako', 1, 0),
-('00000000000000000000000003', NULL, '00000000000000000000000003', 'Three days left until the hackathon', 0, 0),
-('00000000000000000000000004', NULL, '00000000000000000000000003', 'This is a test post', 0, 0),
-('00000000000000000000000005', NULL, '00000000000000000000000001', 'This is Hanako', 0, 0),
-('00000000000000000000000006', NULL, '00000000000000000000000001', 'This is a longer test post. The character limit is 200 characters. What is the actual Twitter character limit? 140 characters?', 0, 0);
+('00000000000000000000000001', NULL, '00000000000000000000000001', 'a   This is Hanako first post', 1, 1),
+('00000000000000000000000002', '00000000000000000000000001', '00000000000000000000000002', 'a-a   This is Taro reply to Hanako', 1, 0),
+('00000000000000000000000003', NULL, '00000000000000000000000003', 'b   Three days left until the hackathon', 0, 0),
+('00000000000000000000000004', NULL, '00000000000000000000000003', 'c   This is a test post', 0, 0),
+('00000000000000000000000005', NULL, '00000000000000000000000001', 'd   This is Hanako', 0, 0),
+('00000000000000000000000006', NULL, '00000000000000000000000001', 'e   This is a longer test post. The character limit is 200 characters. What is the actual Twitter character limit? 140 characters?', 0, 0);
